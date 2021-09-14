f360f26b4a6c702db8fd95681a6e59591b886991
unknown <goyandle@gmail.com>
2021-09-13 22:28:00 -0400

    Add letter template creation, add name list creation, add letters_to_send creation loop.

--- Day 24 - Files Directories and Paths/Mail Merge Challenge/main.py
+++ Day 24 - Files Directories and Paths/Mail Merge Challenge/main.py
@@ -7,14 +7,22 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+# Generates template letter from file.
 with open("Input/Letters/starting_letter.txt", mode= "r") as example:
     template = example.read()
 
+# Creates name list from file.
 with open("Input/Names/invited_names.txt", mode= "r") as name_list:
-    names = name_list.read()
+    names = name_list.readlines(-1)
+    names = [name.rstrip() for name in names]
 
-
-
+# Inserts the name into the template per person, resets template at end.
 for name in names:
-    with open(f"Output/ReadyToSend/{name}'s_letter.txt", mode= "r") as custom_letter:
+     with open(f"Output/ReadyToSend/{name}'s_letter.txt", mode= "w") as custom_letter:
+         template = template.replace("[name]", name)
+         custom_letter.write(template)
+         with open("Input/Letters/starting_letter.txt", mode="r") as example:
+             template = example.read()
+
+
 
