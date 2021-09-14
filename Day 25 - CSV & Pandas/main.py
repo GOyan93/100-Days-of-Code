db3bfec5348a6e21b070cf9ca1dccb7ed34c73ae
unknown <goyandle@gmail.com>
2021-09-14 19:57:31 -0400

    Calculate the max temp

--- Day 25 - CSV & Pandas/main.py
+++ Day 25 - CSV & Pandas/main.py
@@ -4,16 +4,21 @@
 #
 # print(data)
 
-import csv
-temperatures = []
-with open("weather_data.csv") as wx_data:
-    data = csv.reader(wx_data)
-    for row in data:
-        print(row)
-        temperatures.append(row[1])
-temperatures = temperatures[1:]
-for i in range(0, len(temperatures)-1):
-    temperatures[i] = int(temperatures[i])
+# import csv
+# import pandas
+#
+# temperatures = []
+# with open("weather_data.csv") as wx_data:
+#     data = csv.reader(wx_data)
+#     for row in data:
+#         print(row)
+#         temperatures.append(row[1])
+# temperatures = temperatures[1:]
+# for i in range(0, len(temperatures)-1):
+#     temperatures[i] = int(temperatures[i])
+#
+# print(temperatures)
+import pandas
 
-print(temperatures)
-
+df = pandas.read_csv("weather_data.csv")
+print(df['temp'])
