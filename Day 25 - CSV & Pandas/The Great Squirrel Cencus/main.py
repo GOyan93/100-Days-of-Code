4634a0e4452afd094e72c65505c331cb87b1a3a5
unknown <goyandle@gmail.com>
2021-09-14 20:20:19 -0400

    Initial commit. Add TODO list

---
+++ Day 25 - CSV & Pandas/The Great Squirrel Cencus/main.py
@@ -0,0 +1,7 @@
+import pandas
+
+# TODO Create a separate CSV file that displays the count of squirrels by colour
+# TODO Filter dataframe for colour
+# TODO Count number of each colour in data frame
+# TODO Create separate csv file containing colour data
+
