94a1b91ecf76529c22112a799c762befb2e910a7
unknown <goyandle@gmail.com>
2021-09-21 19:16:15 -0500

    Final commit for Day 27, Mile to Kilometer Converter

--- Day 27 - Tkinter Args & Kwargs/Mile to Kilometer Converter/main.py
+++ Day 27 - Tkinter Args & Kwargs/Mile to Kilometer Converter/main.py
@@ -1,17 +1,18 @@
 import tkinter as tk
 main_window = tk.Tk()
 main_window.title("Mile to Km Converter")
+main_window.config(padx=20, pady=20)
 
 
 # Functions
 def conversion():
     user_input = entry_user.get()
     converted = float(user_input) * 1.609
-    lbl_answer.config(text=converted)
+    lbl_answer.config(text=str(converted))
 
 
 # Create widgets for window
-entry_user = tk.Entry()
+entry_user = tk.Entry(text= "0")
 lbl_miles = tk.Label(text= "Miles")
 lbl_is_equal = tk.Label(text= "is equal to")
 lbl_answer = tk.Label(text= "0")
@@ -19,12 +20,12 @@ lbl_Km = tk.Label(text= "Km")
 button_calculate = tk.Button(text= "Calculate", command= conversion)
 
 # Label Placement
-entry_user.grid(row= 1, column= 2)
-lbl_miles.grid(row=1, column=3)
-lbl_is_equal.grid(row=2, column=1)
-lbl_answer.grid(row=2, column=2)
-lbl_Km.grid(row=2, column=3)
-button_calculate.grid(row=3, column=2)
+entry_user.grid(row= 0, column= 1)
+lbl_miles.grid(row=0, column=2)
+lbl_is_equal.grid(row=1, column=0)
+lbl_answer.grid(row=1, column=1)
+lbl_Km.grid(row=1, column=2)
+button_calculate.grid(row=2, column=1)
 
 tk.mainloop()
 
