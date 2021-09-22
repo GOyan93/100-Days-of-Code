ea0599adebfb38e0509131c27999f67b0a365f16
unknown <goyandle@gmail.com>
2021-09-21 19:28:31 -0500

    Add padding to tomato

--- Day 28 -Tkinter Dynamic Typing and Pomodoro Timer/Pomodoro Timer/main.py
+++ Day 28 -Tkinter Dynamic Typing and Pomodoro Timer/Pomodoro Timer/main.py
@@ -19,10 +19,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx= 100, pady= 50)
 
 canvas = Canvas(width= 200, height= 224)
 tomato_png = PhotoImage(file= "tomato.png")
-canvas.create_image(100, 112, image= tomato_png)
+canvas.create_image(103, 112, image= tomato_png)
 canvas.pack()
 
 window.mainloop()
