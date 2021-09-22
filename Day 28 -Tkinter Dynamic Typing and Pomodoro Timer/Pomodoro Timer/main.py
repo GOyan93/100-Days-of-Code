4d37ea9a5916cf2757e5ab7607e4f5854932bca9
unknown <goyandle@gmail.com>
2021-09-21 21:29:36 -0500

    Add break functionality to count_down function

--- Day 28 -Tkinter Dynamic Typing and Pomodoro Timer/Pomodoro Timer/main.py
+++ Day 28 -Tkinter Dynamic Typing and Pomodoro Timer/Pomodoro Timer/main.py
@@ -9,23 +9,46 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label_timer_title.config(text="Break", fg= RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label_timer_title.config(text="Break", fg=RED)
+    else:
+        count_down(work_sec)
+        label_timer_title.config(text="Work", fg=GREEN)
+
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-
+    global reps
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
+        window.after(2, count_down, count-1)
+
+    if count == 0:
+        start_timer()
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
