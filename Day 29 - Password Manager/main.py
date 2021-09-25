32593b0df91f3d617b40f972afafdbf88a156a83
unknown <goyandle@gmail.com>
2021-09-24 20:17:19 -0500

    Add save function

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -1,13 +1,26 @@
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    global entry_website, entry_email, entry_password
+    user_website = entry_website.get()
+    user_email = entry_email.get()
+    user_password = entry_password.get()
+    with open("data.txt.", mode="a") as txt_file:
+        txt_file.write(f"{user_website} | {user_email} | {user_password}\n")
+    entry_website.delete(0, END)
+    entry_email.delete(0, END)
+    entry_password.delete(0, END)
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
 
 window = Tk()
-window.minsize(200, 200)
-window.config(padx= 20, pady= 20)
+#window.minsize(200, 200)
+window.config(padx= 50, pady= 20)
 window.title("Password Manager")
 lock_img = PhotoImage(file= "logo.png")
 canvas = Canvas(height= 200, width = 200)
@@ -20,14 +33,16 @@ label_password = Label(text= "Password:")
 entry_website = Entry(width= 35, justify="left")
 entry_email = Entry(width= 35, justify="left")
 entry_password = Entry(width= 21, justify="left")
-button_add = Button(text= "Add", width= 36, justify= "left")
+button_add = Button(text= "Add", width= 36, justify= "left", command= save)
 button_generate = Button(text= "Generate Password")
 
 
 # Widget Layout
 canvas.grid(row= 0, column= 1)
 entry_website.grid(row=1, column= 1, columnspan= 2, ipadx= 50)
+entry_website.focus()
 entry_email.grid(row=2, column= 1, columnspan= 2, ipadx= 50)
+entry_email.insert(0, "example@email.com")
 entry_password.grid(row=3, column= 1, ipadx= 35)
 label_website.grid(row= 1, column= 0)
 label_email.grid(row= 2, column= 0)
