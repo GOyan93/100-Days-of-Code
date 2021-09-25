0469c3c77e97fcab07574a38d9a464e30e8f639c
unknown <goyandle@gmail.com>
2021-09-24 22:01:05 -0400

    Add password generate function

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -1,7 +1,33 @@
 from tkinter import *
 from tkinter import messagebox
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
 
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    entry_password.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list += [random.choice(letters) for char in range(nr_letters)]
+
+    password_list += [random.choice(symbols) for char in range(nr_symbols)]
+
+    password_list += [random.choice(numbers) for char in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+      password += char
+
+    entry_password.insert(0, f"{password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     global entry_website, entry_email, entry_password
@@ -11,14 +37,13 @@ def save():
 
     if len(user_website) == 0 or len(user_email) == 0 or len(user_password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
-        is_ok = False
     else:
         is_ok = messagebox.askokcancel(title=user_website, message= f"These are the details entered: \nEmail: {user_email} \nPassword: {user_password} \nIs it ok to save?")
-    if is_ok:
-        with open("data.txt.", mode="a") as txt_file:
-            txt_file.write(f"{user_website} | {user_email} | {user_password}\n")
-        entry_website.delete(0, END)
-        entry_password.delete(0, END)
+        if is_ok:
+            with open("data.txt.", mode="a") as txt_file:
+                txt_file.write(f"{user_website} | {user_email} | {user_password}\n")
+            entry_website.delete(0, END)
+            entry_password.delete(0, END)
 
 
 
@@ -41,7 +66,7 @@ entry_website = Entry(width= 35, justify="left")
 entry_email = Entry(width= 35, justify="left")
 entry_password = Entry(width= 21, justify="left")
 button_add = Button(text= "Add", width= 36, justify= "left", command= save)
-button_generate = Button(text= "Generate Password")
+button_generate = Button(text= "Generate Password", command= generate)
 
 
 # Widget Layout
