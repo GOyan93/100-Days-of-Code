9aaced5272c5a8895151e58ec049cdc38c8fc3ef
unknown <goyandle@gmail.com>
2021-09-25 21:02:08 -0400

    Add search button. Bug: Unexpected layout shift

--- Day 30 - Error Management/Update Password Manager/main.py
+++ Day 30 - Error Management/Update Password Manager/main.py
@@ -57,7 +57,8 @@ def save():
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
-
+def search():
+    pass
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -78,7 +79,7 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=35)
@@ -88,9 +89,12 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", command=search)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
