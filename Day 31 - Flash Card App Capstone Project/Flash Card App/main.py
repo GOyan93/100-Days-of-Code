3cdb7c55f15d56dec136779d549d2117483044fb
unknown <goyandle@gmail.com>
2021-09-27 00:42:57 -0400

    Add generate random french word from CSV

--- Day 31 - Flash Card App Capstone Project/Flash Card App/main.py
+++ Day 31 - Flash Card App Capstone Project/Flash Card App/main.py
@@ -1,7 +1,24 @@
 from tkinter import *
+import pandas as pd
+import random
 
 # Variables
 BACKGROUND_COLOR = "#B1DDC6"
+df = pd.read_csv("data/french_words.csv")
+list_word_pairs = df.to_dict("records")
+
+
+### FUNCTIONS ###
+
+def generate_word():
+    chosen_pair = random.choice(list_word_pairs)
+    french_word = chosen_pair['French']
+    english_word = chosen_pair["English"]
+    canvas.itemconfig(displayed_word, text=f"{french_word}")
+
+
+
+
 
 ### UI INTERFACE ###
 window = Tk()
@@ -19,11 +36,11 @@ image_wrong_button = PhotoImage(file= "images/wrong.png")
 canvas = Canvas(width= 800, height= 525)
 canvas.config(bg = BACKGROUND_COLOR, highlightthickness= 0)
 canvas.create_image(400, 251, image= image_card_front)
-canvas.create_text(400, 150, text="French", font= ("Arial", 40, "italic"))
-canvas.create_text(400, 263, text= "trouve", font= ("Arial", 40, "bold"))
+title = canvas.create_text(400, 150, text="French", font= ("Arial", 40, "italic"))
+displayed_word = canvas.create_text(400, 263, text= "trouve", font= ("Arial", 40, "bold"))
 
-button_correct = Button(image= image_correct_button, bg = BACKGROUND_COLOR, highlightthickness= 0, relief = "flat")
-button_wrong = Button(image= image_wrong_button, bg = BACKGROUND_COLOR, highlightthickness= 0, relief= "flat")
+button_correct = Button(image= image_correct_button, bg = BACKGROUND_COLOR, highlightthickness= 0, relief= "flat", command=generate_word)
+button_wrong = Button(image= image_wrong_button, bg = BACKGROUND_COLOR, highlightthickness= 0, relief= "flat", command=generate_word)
 
 
 # UI Widget Layout
